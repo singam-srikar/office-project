237571d02cd1130ffd0ef70ff614cf5a4fbf9c3e
singam-srikar <singamsrikarsingam@gmail.com>
2026-07-03 22:16:55 +0530

    Srikar | Add multiple exceptions

--- src/main/java/com/projects/office/advice/ApiError.java
+++ src/main/java/com/projects/office/advice/ApiError.java
@@ -5,11 +5,13 @@ import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
 public class ApiError {
     private HttpStatus httpStatus;
     private String message;
+    private List<String> subErrors;
     private LocalDateTime timestamp;
 }
--- src/main/java/com/projects/office/advice/GlobalExceptionHandling.java
+++ src/main/java/com/projects/office/advice/GlobalExceptionHandling.java
@@ -1,13 +1,14 @@
 package com.projects.office.advice;
 
-import com.projects.office.dto.DepartmentDTO;
 import com.projects.office.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandling {
@@ -20,5 +21,33 @@ public class GlobalExceptionHandling {
                 .timestamp(LocalDateTime.now())
                 .build();
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleAllException(Exception exception){
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //to handle multiple input validation
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
+       List<String> errorList =  methodArgumentNotValidException.
+                getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .toList();
+        ApiError apiError = ApiError.builder()
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .message("Invalid input")
+                .subErrors(errorList)
+                .timestamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+
     }
 }
